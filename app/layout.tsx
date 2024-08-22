7bc7c13b6e48bdd508cc195ec5ea4d8a1b396e23
Rodrigo B <rodrigobs.uff@gmail.com>
2024-08-22 15:16:55 -0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import {Providers} from "./providers";
-import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const inter = Inter({ subsets: ['latin'] })
 
